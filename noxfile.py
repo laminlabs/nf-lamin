88713ceae176d096266d5f0b0c8e1c8d9f26453c
Lukas Heumos <lukas.heumos@posteo.net>
2025-04-08 11:58:38 +0200

    ✨ Create docs again
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

---
+++ noxfile.py
@@ -0,0 +1,18 @@
+import nox
+from laminci import upload_docs_artifact
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run
+
+nox.options.default_venv_backend = "none"
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session()
+def build(session):
+    run(session, "uv pip install laminci nbproject-test")
+    login_testuser1(session)
+    build_docs(session)
+    upload_docs_artifact(aws=True)
