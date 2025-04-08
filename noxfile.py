d4d7a914cb3e42dd5c46177c8dda4759a109a4e0
Lukas Heumos <lukas.heumos@posteo.net>
2025-04-08 12:20:09 +0200

    🎨 Fix system
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,7 @@ def lint(session: nox.Session) -> None:
 
 @nox.session()
 def build(session):
-    run(session, "uv pip install laminci nbproject-test")
+    run(session, "uv pip install --system laminci nbproject-test")
     login_testuser1(session)
     build_docs(session)
     upload_docs_artifact(aws=True)
