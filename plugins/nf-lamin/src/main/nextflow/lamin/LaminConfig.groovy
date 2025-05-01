680aede5eadd1bc0d750f121572f61f5e154075e
Robrecht Cannoodt <rcannood@gmail.com>
2025-05-01 14:53:00 +0200

    add project setting, load settings from environment variables

--- plugins/nf-lamin/src/main/nextflow/lamin/LaminConfig.groovy
+++ plugins/nf-lamin/src/main/nextflow/lamin/LaminConfig.groovy
@@ -14,8 +14,9 @@ import nextflow.Session
  * These settings can be defined in the nextflow config as follows:
  *
  * lamin {
- *   instance = "laminlabs/lamindata"
- *   api_key = System.getenv("LAMIN_API_KEY")
+ *   instance = 'laminlabs/lamindata'
+ *   api_key = System.getenv('LAMIN_API_KEY')
+ *   project = System.getenv('LAMIN_CURRENT_PROJECT')
  * }
  *
  * @author Robrecht Cannoodt <robrecht@data-intuitive.com>
@@ -35,34 +36,35 @@ class LaminConfig {
     final protected String apiKey
 
     /**
-     * The dry run flag
+     * The project for the Lamin API
      */
-    final protected Boolean dryRun
+    final protected String project
 
     /**
      * Configuration for Lamin API integration
-     * @param instance format: "owner/repo"
+     * @param instance format: 'owner/repo'.
      * @param apiKey LaminDB API authorization key
-     * @param dryRun whether to execute in dry run mode
+     * @param project the project for the Lamin API
+     * @throws IllegalArgumentException if any of the parameters are null or invalid
      */
-    LaminConfig(String instance, String apiKey, Boolean dryRun = false) {
+    LaminConfig(String instance, String apiKey, String project = null) {
         // check if all values are available
         if (!instance?.trim()) {
-            throw new IllegalArgumentException("Lamin instance is not set. Please set the 'lamin.instance' in your nextflow.config file.")
+            throw new IllegalArgumentException('Lamin instance is not set. Please set the "lamin.instance" in your nextflow.config file.')
         }
         if (!apiKey?.trim()) {
-            throw new IllegalArgumentException("Lamin API key is not set. Please set the 'lamin.api_key' in your nextflow.config file.")
+            throw new IllegalArgumentException('Lamin API key is not set. Please set the "lamin.api_key" in your nextflow.config file.')
         }
 
         // check if instance is <owner>/<repo>
         if (!instance.matches(/^[\w.-]+\/[\w.-]+$/)) {
-            throw new IllegalArgumentException("Provided Lamin instance ${instance} is not valid. Please provide a valid instance in the format <owner>/<repo>.")
+            throw new IllegalArgumentException('Provided Lamin instance ${instance} is not valid. Please provide a valid instance in the format <owner>/<repo>.')
         }
 
         // store values
         this.instance = instance
         this.apiKey = apiKey
-        this.dryRun = dryRun ?: false
+        this.project = project
     }
 
     /**
@@ -98,15 +100,15 @@ class LaminConfig {
     }
 
     /**
-     * Get the dry run flag
-     * @return the dry run flag
+     * Get the project for the Lamin API
+     * @return the project
      */
-    Boolean isDryRun() {
-        return this.dryRun
+    String getProject() {
+        return this.project
     }
 
     /**
-     * Create a LaminConfig object from the Nextflow session
+     * Create a LaminConfig object from the Nextflow session and environment variables
      * @param session the Nextflow session
      * @return a LaminConfig object
      */
@@ -116,9 +118,10 @@ class LaminConfig {
             throw new IllegalArgumentException('Session is null. Please provide a valid session.')
         }
         Map map = session.config?.navigate('lamin') as Map ?: [:]
-        String instance = map.instance
-        String apiKey = map.api_key
-        return new LaminConfig(instance, apiKey)
+        String instance = map.instance ?: System.getenv('LAMIN_CURRENT_INSTANCE')
+        String apiKey = map.api_key ?: System.getenv('LAMIN_API_KEY')
+        String project = map.project ?: System.getenv('LAMIN_CURRENT_PROJECT')
+        return new LaminConfig(instance, apiKey, project)
     }
 
 }
