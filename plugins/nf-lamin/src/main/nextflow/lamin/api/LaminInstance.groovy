997fb26498f8a8bcdd001e2c22ae05d0be46913e
Robrecht Cannoodt <rcannood@gmail.com>
2025-05-01 14:54:45 +0200

    Add createRecord

--- plugins/nf-lamin/src/main/nextflow/lamin/api/LaminInstance.groovy
+++ plugins/nf-lamin/src/main/nextflow/lamin/api/LaminInstance.groovy
@@ -86,23 +86,38 @@ class LaminInstance {
 
     /**
      * Get a record from the Lamin API
-     * @param moduleName the module name
-     * @param modelName the model name
-     * @param idOrUid the ID or UID of the record
-     * @param limitToMany the limit to many
-     * @param includeForeignKeys whether to include foreign keys
-     * @param getRecordRequestBody the request body
-     * @return the record
+     * @param args A map containing the following keys:
+     *    - moduleName: The name of the module (required)
+     *    - modelName: The name of the model (required)
+     *    - idOrUid: The ID or UID of the record (required)
+     *    - limitToMany: The limit for many records (optional, default: 10)
+     *    - includeForeignKeys: Whether to include foreign keys in the response (optional, default: false)
+     *    - select: A list of fields to select (optional)
+     * @return a map containing the record data
+     * @throws IllegalStateException if any of the required arguments are null
      * @throws ApiException if an error occurs while fetching the record
      */
-    Object getRecord(
-        String moduleName,
-        String modelName,
-        String idOrUid,
-        Integer limitToMany = 10,
-        Boolean includeForeignKeys = false,
-        GetRecordRequestBody body = new GetRecordRequestBody()
-    ) throws ApiException {
+    Map getRecord(Map args) throws ApiException {
+        // required args
+        String moduleName = args.moduleName as String
+        String modelName = args.modelName as String
+        String idOrUid = args.idOrUid as String
+
+        if (!moduleName) { throw new IllegalStateException('Module name is null. Please check the module name.') }
+        if (!modelName) { throw new IllegalStateException('Model name is null. Please check the model name.') }
+        if (!idOrUid) { throw new IllegalStateException('ID or UID is null. Please check the ID or UID.') }
+
+        // Optional arguments
+        Integer limitToMany = args.get('limitToMany', 10) as Integer
+        Boolean includeForeignKeys = args.get('includeForeignKeys', false) as Boolean
+        List<String> select = args.get('select', null) as List<String>
+
+        // Create the request body
+        GetRecordRequestBody body = new GetRecordRequestBody(
+            select: select
+        )
+
+        // Do call
         return callApi { String accessToken ->
             this.apiInstance.getRecordInstancesInstanceIdModulesModuleNameModelNameIdOrUidPost(
                 moduleName,
@@ -114,19 +129,55 @@ class LaminInstance {
                 this.settings.schemaId(),
                 accessToken,
                 body
-            )
+            ) as Map
         }
     }
 
-    Object getRecords(
-        String moduleName,
-        String modelName,
-        Integer limit = 50,
-        Integer offset = 0,
-        Integer limitToMany = 10,
-        Boolean includeForeignKeys = false,
-        GetRecordsRequestBody body = new GetRecordsRequestBody()
+    /**
+     * Get records from the Lamin API
+     * @param args A map containing the following keys:
+     *    - moduleName: The name of the module (required)
+     *    - modelName: The name of the model (required)
+     *    - limit: The limit for the number of records (optional, default: 50)
+     *    - offset: The offset for pagination (optional, default: 0)
+     *    - limitToMany: The limit for many records (optional, default: 10)
+     *    - includeForeignKeys: Whether to include foreign keys in the response (optional, default: false)
+     *    - search: The search term (optional)
+     *    - orderBy: The order by clause (optional)
+     *    - filter: The filter clause (optional)
+     *    - select: A list of fields to select (optional)
+     * @return a list of maps containing the record data
+     * @throws IllegalStateException if any of the required arguments are null
+     * @throws ApiException if an error occurs while fetching the records
+     */
+    List<Map> getRecords(
+        Map args
     ) throws ApiException {
+        // required args
+        String moduleName = args.moduleName as String
+        String modelName = args.modelName as String
+        if (!moduleName) { throw new IllegalStateException('Module name is null. Please check the module name.') }
+        if (!modelName) { throw new IllegalStateException('Model name is null. Please check the model name.') }
+
+        // Optional arguments
+        Integer limit = args.get('limit', 50) as Integer
+        Integer offset = args.get('offset', 0) as Integer
+        Integer limitToMany = args.get('limitToMany', 10) as Integer
+        Boolean includeForeignKeys = args.get('includeForeignKeys', false) as Boolean
+        String search = args.get('search', null) as String
+        List<OrderByColumn> orderBy = args.get('orderBy', null) as List<OrderByColumn>
+        Map<String, Object> filter = args.get('filter', null) as Map<String, Object>
+        List<String> select = args.get('select', null) as List<String>
+        
+        // Create the request body
+        GetRecordsRequestBody body = new GetRecordsRequestBody(
+            select: select,
+            filter: filter,
+            orderBy: orderBy,
+            search: search
+        )
+
+        // Do call
         return callApi { String accessToken ->
             this.apiInstance.getRecordsInstancesInstanceIdModulesModuleNameModelNamePost(
                 moduleName,
@@ -139,7 +190,43 @@ class LaminInstance {
                 this.settings.schemaId(),
                 accessToken,
                 body
-            )
+            ) as List<Map>
+        }
+    }
+
+    /**
+     * Create a record in the Lamin API
+     * @param args A map containing the following keys:
+     *    - moduleName: The name of the module (required)
+     *    - modelName: The name of the model (required)
+     *    - data: The data to create the record (required)
+     * @return a map containing the created record data
+     * @throws IllegalStateException if any of the required arguments are null
+     * @throws ApiException if an error occurs while creating the record
+     */
+    Map createRecord(
+        Map args
+    ) throws ApiException {
+        // required args
+        String moduleName = args.moduleName as String
+        String modelName = args.modelName as String
+        if (!moduleName) { throw new IllegalStateException('Module name is null. Please check the module name.') }
+        if (!modelName) { throw new IllegalStateException('Model name is null. Please check the model name.') }
+
+        // Optional arguments
+        // drop moduleName and modelName from body
+        Map data = args.get('data', null) as Map
+
+        // Do call
+        return callApi { String accessToken ->
+            this.apiInstance.createRecordInstancesInstanceIdModulesModuleNameModelNamePut(
+                moduleName,
+                modelName,
+                this.settings.id(),
+                data,
+                this.settings.schemaId(),
+                accessToken
+            ) as Map
         }
     }
 
