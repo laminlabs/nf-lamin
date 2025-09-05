ad419c1c3c7eaec4db6cf99cb2beadb2007f967f
Robrecht Cannoodt <rcannood@gmail.com>
2025-09-05 13:22:22 +0200

    Add link to comment

--- plugins/nf-lamin/src/main/nextflow/lamin/model/RunStatus.groovy
+++ plugins/nf-lamin/src/main/nextflow/lamin/model/RunStatus.groovy
@@ -5,6 +5,8 @@ import groovy.transform.CompileStatic
 /**
  * Status codes for workflow runs in Lamin
  *
+ * See https://github.com/laminlabs/lamindb/blob/768c66c6347e1a098ffd0b9e03f77685c47f679c/lamindb/models/run.py#L323
+ *
  * SCHEDULED: When workflow is created (onFlowCreate)
  * RESTARTED: For workflow restarts (future use)
  * STARTED: When workflow begins execution (onFlowBegin)
