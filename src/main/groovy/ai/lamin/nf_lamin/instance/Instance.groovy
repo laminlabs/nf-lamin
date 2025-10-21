a82d4940a25c08c038326d6fc4e1836d7bb1ca0a
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2025-10-21 13:36:59 +0200

    Implement methods to fetch artifact information
    add placeholder testbench to test code while developing. Will have to be cleaned up or restructured later.

--- src/main/groovy/ai/lamin/nf_lamin/instance/Instance.groovy
+++ src/main/groovy/ai/lamin/nf_lamin/instance/Instance.groovy
@@ -11,6 +11,8 @@ import ai.lamin.lamin_api_client.api.DefaultApi
 
 import ai.lamin.nf_lamin.hub.LaminHub
 
+import java.nio.file.Paths
+
 /**
  * Represents a Lamin instance.
  * This class is responsible for interacting with the Lamin API.
@@ -525,6 +527,64 @@ class Instance {
             throw new IllegalStateException("Failed to upload artifact. Response: ${response}")
         }
         return responseBody?.artifact as Map<String, Object>
+    }
+
+    String getArtifactUrl(String uid) throws ApiException {
+
+        if (!uid) {
+            throw new IllegalStateException('UID is null or empty. Please check the UID.')
+        }
+        if (uid.length() != 16 && uid.length() != 20) {
+            throw new IllegalStateException("UID '${uid}' is not valid. It should be 16 or 20 characters long.")
+        }
+
+    /*
+     *    - moduleName: The name of the module (required)
+     *    - modelName: The name of the model (required)
+     *    - limit: The limit for the number of records (optional, default: 50)
+     *    - offset: The offset for pagination (optional, default: 0)
+     *    - limitToMany: The limit for many records (optional, default: 10)
+     *    - includeForeignKeys: Whether to include foreign keys in the response (optional, default: false)
+     *    - search: The search term (optional)
+     *    - orderBy: The order by clause (optional)
+     *    - filter: The filter clause (optional)
+     *    - select: A list of fields to select (optional)
+     */
+        Map args = [
+            moduleName: 'core',
+            modelName: 'artifact',
+            limit: 1,
+            offset: 0,
+            orderBy: [['field': 'updated_at', 'descending': true]],
+            filter: [
+                'uid': ['startswith': uid]
+            ]
+        ]
+        List<Map> records = getRecords(args)
+
+        if (records.size() == 0) {
+            throw new ApiException("No artifact found with uid starting with '${uid}'")
+        }
+        log.info "Found ${records.size()} artifacts with uid starting with '${uid}'"
+        Map artifact = records[0]
+
+        log.warn 'WARNING: getArtifactUrl() currently returns a hardcoded URL for testing purposes. Implement proper URL retrieval logic.'
+        String instancePath = 's3:/lamindata/.lamindb/'
+        log.info "Using artifact: ${artifact}"
+
+        String versioned_uid = artifact.uid as String
+        if (versioned_uid.length() != 20) {
+            throw new IllegalStateException("Artifact UID '${versioned_uid}' is not valid. It should be 20 characters long.")
+        }
+        String suffix = artifact.suffix as String
+        if (suffix == null) {
+            throw new IllegalStateException("Artifact suffix is null. Please check the artifact data.")
+        }
+
+        String combined = Paths.get(instancePath).resolve("${versioned_uid}${suffix}").toString()
+        log.info "Constructed artifact URL: ${combined}"
+
+        return combined
     }
 
     // ------------------- PRIVATE METHODS -------------------
