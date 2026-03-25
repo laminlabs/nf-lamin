170a6bf21dba04f3f4d510cc632318702989dd45
Robrecht Cannoodt <rcannood@gmail.com>
2026-03-25 15:08:08 +0100

    use uv if it is available

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,10 @@
+import shutil
+
 import nox
 from laminci import upload_docs_artifact
 from laminci.nox import build_docs, run_pre_commit
 
-nox.options.default_venv_backend = "uv"
+nox.options.default_venv_backend = "uv" if shutil.which("uv") else "none"
 
 
 @nox.session
