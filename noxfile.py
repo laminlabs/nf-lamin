9f4f16ce00b7f4959dad8c07427ff9431ac3dd66
Robrecht Cannoodt <rcannood@gmail.com>
2026-03-25 15:53:08 +0100

    try alternative approach

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,16 @@
-import shutil
+import os
 
 import nox
 from laminci import upload_docs_artifact
 from laminci.nox import build_docs, run_pre_commit
 
-nox.options.default_venv_backend = "uv" if shutil.which("uv") else "none"
+IS_CI = "CI" in os.environ
+
+# we'd like to aggregate coverage information across sessions
+# and for this the code needs to be located in the same
+# directory in every github action runner
+# this also allows to break out an installation section
+nox.options.default_venv_backend = "none" if IS_CI else "uv"
 
 
 @nox.session
