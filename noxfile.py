2f883314b424d9eafe4a379a58441b6af45361d2
Robrecht Cannoodt <rcannood@gmail.com>
2026-03-25 08:37:05 +0100

    simplify the docs build for now, since there are no longer any ipynb builds. can always add it back later

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,11 @@
 import os
-import shutil
 from pathlib import Path
 
 import nox
-from laminci import convert_executable_md_files, upload_docs_artifact
-from laminci.nox import build_docs, login_testuser1, run, run_pre_commit, run_pytest
+from laminci import upload_docs_artifact
+from laminci.nox import build_docs, run_pre_commit
 
-# we'd like to aggregate coverage information across sessions
-# and for this the code needs to be located in the same
-# directory in every github action runner
-# this also allows to break out an installation section
 nox.options.default_venv_backend = "none"
-
-IS_PR = os.getenv("GITHUB_EVENT_NAME") != "push"
-
-GROUPS = {}
-GROUPS["postrun"] = ["nextflow-postrun.ipynb"]
-GROUPS["plugin"] = ["nextflow.ipynb"]
 
 
 @nox.session
@@ -25,40 +14,6 @@ def lint(session: nox.Session) -> None:
 
 
 @nox.session
-@nox.parametrize(
-    "group",
-    [
-        "postrun",
-        "plugin",
-    ],
-)
-def build(session, group):
-    session.run(
-        "uv",
-        "pip",
-        "install",
-        "--system",
-        "lamindb[jupyter,bionty]",
-    )
-    session.run(*"pip install -e .[dev]".split())
-    convert_executable_md_files("./docs")
-    login_testuser1(session)
-    run(session, f"pytest -s ./tests/test_notebooks.py::test_{group}")
-
-    # move artifacts into right place
-    target_dir = Path(f"./docs_{group}")
-    target_dir.mkdir(exist_ok=True)
-    for filename in GROUPS[group]:
-        shutil.copy(Path("docs") / filename, target_dir / filename)
-
-
-@nox.session
 def docs(session):
-    for group in [
-        "postrun",
-        "plugin",
-    ]:
-        for path in Path(f"./docs_{group}").glob("*"):
-            path.rename(f"./docs/{path.name}")
     build_docs(session, strict=False)
     upload_docs_artifact(aws=True)
