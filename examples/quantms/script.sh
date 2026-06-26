6f8083d2b2b3fb40ef0b499c47daae892d7be5fc
Robrecht Cannoodt <rcannood@gmail.com>
2026-06-26 10:48:24 +0200

    update configs and scripts

--- examples/quantms/script.sh
+++ examples/quantms/script.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-export NXF_VER=25.10.4
+export TOWER_WORKSPACE_ID="14874785379374"
+export NXF_VER=25.10.5
+export NXF_SYNTAX_PARSER=v1
+
+# uncomment this for production
+CONFIG=configs/env-prod.config
+OUTDIR="$LAMIN_TEST_BUCKET/scrnaseq/run_$(date +%Y%m%d_%H%M%S)"
+
+# uncomment this for staging
+CONFIG=configs/env-staging.config
+OUTDIR="${LAMIN_TEST_BUCKET}-staging/scrnaseq/run_$(date +%Y%m%d_%H%M%S)"
+
 nextflow \
   -trace ai.lamin \
   run bigbio/quantms \
   -r 1.7.0 \
   -profile docker,test_lfq \
   -c examples/quantms/nextflow.config \
-  --outdir output/quantms/
+  -c $CONFIG \
+  --outdir $OUTDIR \
+  -with-tower
--- examples/scrnaseq/script.sh
+++ examples/scrnaseq/script.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-export NXF_VER=25.10.4
-nextflow run nf-core/scrnaseq \
+export TOWER_WORKSPACE_ID="14874785379374"
+export NXF_VER=25.10.5
+export NXF_SYNTAX_PARSER=v1
+
+# uncomment this for production
+CONFIG=configs/env-prod.config
+OUTDIR="$LAMIN_TEST_BUCKET/scrnaseq/run_$(date +%Y%m%d_%H%M%S)"
+
+# uncomment this for staging
+CONFIG=configs/env-staging.config
+OUTDIR="${LAMIN_TEST_BUCKET}-staging/scrnaseq/run_$(date +%Y%m%d_%H%M%S)"
+
+nextflow \
+  -trace ai.lamin \
+  run nf-core/scrnaseq \
   -r 4.1.0 \
   -profile docker,test \
   -c examples/scrnaseq/nextflow.config \
-  --outdir output/scrnaseq/
+  -c $CONFIG \
+  --outdir $OUTDIR \
+  -with-tower
